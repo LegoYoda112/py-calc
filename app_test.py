dfff29da6382d30631de48b8ffa8be6c1f63f17d
Thomas G <thomas.godden@outlook.com>
2022-12-30 15:45:47 -0800

    Initial commit part 2

---
+++ app_test.py
@@ -0,0 +1,65 @@
+import eel
+import calc_parser
+import time
+import os
+
+# Start eel
+eel.init('web', allowed_extensions=['.js', '.html'])
+
+# List files in save directory
+@eel.expose
+def get_file_names():
+    files = os.listdir('saves')
+    # Sort by modified datetime
+    files.sort(key=lambda x: -os.path.getmtime('saves/' + x))
+    return files
+
+# Return file text given path
+@eel.expose
+def get_file_text(file_path):
+    with open('saves/' + file_path, 'r') as file:
+        return file.read()
+
+# Saves a file given the path and text
+@eel.expose
+def save_file_text(file_path, text):
+    with open('saves/' + file_path, 'w') as file:
+        return file.write(text)
+
+# Send the input from javascript to python
+# Writes output back into javascript through eel
+@eel.expose
+def send_input(input_string):
+
+    output = ""
+    context = None
+    error = False
+    time_elapsed = 0.0
+
+    try:
+        
+        # Time execution
+        start_time = time.time()
+
+        # Parse, calculate and store output
+        output, context = calc_parser.parse(input_string, True)
+        time_elapsed = time.time() - start_time
+
+    # Print errors
+    # TODO: Pass this back to the app
+    except Exception as e:
+        output = str(e)
+        print(e)
+        error = True
+    
+    # Set diagnostics
+    eel.set_timer(round(time_elapsed * 1000))
+    if(context != None):
+        eel.set_var_num(len(context))
+
+    # Write the output
+    print(error)
+    eel.write_output(output, error)
+
+# Start the app
+eel.start('app.html')
---
+++ calc_parser.py
@@ -0,0 +1,135 @@
+import time
+import re
+from math import sqrt, sin, cos
+import math
+import numpy as np
+from pint import UnitRegistry
+
+# Set up units
+ureg = UnitRegistry()
+
+def parse(input_text, output_only = False):
+    # TODO: Make this a class?
+
+    # Holds current values of all variables
+    context = {}
+
+    # Holds output text
+    output_text = ""
+
+    # Iterate over each input line going from top to bottom
+    for line in input_text.splitlines():
+        output_line = ""
+
+        # If output_only, replace input characters with spaces
+        if(output_only):
+            output_line = ' '*(len(line) + 1)
+        else:
+            output_line = line + ' '
+
+        # Print line for debug purposes
+        # print(">", line)
+
+        # Insert spaces between ( _ )
+        line = re.sub("(\()(?=[0-9a-zA-Z])", "( ", line)
+        line = re.sub("(\))(?!=[0-9a-zA-Z])", " )", line)
+
+        # Pad control characters
+        chars_to_pad = ('\=', '\+', '\*', '\[', '\]', '\,')
+        for char in chars_to_pad:
+            line = re.sub(char, " " + char[-1] + " ", line)
+
+        # Does something to arrays, figure out what?
+        line = re.sub('[ ]+', " ", line)
+
+        # Split line by spaces
+        line_split = line.split()
+        
+        # EMPTY LINE
+        if(len(line_split) == 0):
+            # Pass
+            pass
+
+        # COMMENT
+        elif(line_split[0] == "#"):
+            # Pass
+            pass
+        
+        # ASSIGNMENT AND EVALUATION
+        else:
+            new_line_split = []
+            assign = None
+            
+            # If we're assigning:
+            # > variable = expression
+            # set assign to true and remove the variable name and the "=" from the eval line
+            if line_split[1] == "=":
+                assign = True
+                eval_line_split = line_split[2:]
+            else:
+                assign = False
+                eval_line_split = line_split
+            
+            # For each element in the split check if it's a variable in our context
+            # if so, replace it with context['variable_name'] so it's evaluated correctly
+            for element in eval_line_split:
+                if element in context.keys():
+                    # print(element)
+                    new_element = "context['" + element + "']"
+
+                    new_line_split.append(new_element)
+                else:
+                    new_line_split.append(element)
+
+            # Join line to feed to eval
+            new_line = ' '.join(new_line_split)
+
+            # print(new_line)
+            
+            # Evaluate line value
+            # TODO: without some filtering, this is bad!
+            eval_value = eval(new_line)
+            
+            # If we're assigning, assign to a new variable in the context
+            if assign:
+
+                # If the value is a list, convert to a np array
+                if type(eval_value) == list:
+                    context[line_split[0]] = np.array(eval_value)
+                else:
+                    context[line_split[0]] = eval_value
+
+                # If it's a number, round for output
+                # Note this doesn't round the value stored in context
+                if(is_number(eval_value)):
+                    eval_value = round(eval_value, 3)
+                
+                # Convert to a string
+                eval_value = str(eval_value)
+
+                # Add to the output line
+                if(not (line_split[1] == "=" and len(line_split) == 3)):
+                    output_line += ' '.join(("=", eval_value))
+            else:
+                
+                # If it's a number, round it
+                if(is_number(eval_value)):
+                    eval_value = round(eval_value, 3)
+                
+                # Convert to string
+                eval_value = str(eval_value)
+                
+                # Add to output
+                output_line += "= " + eval_value
+        
+        # Add output line 
+        output_text += output_line + "\n"
+        # print(' ' + output_line[0:-1])
+    
+    # Return output text and variable context
+    return output_text, context
+
+# Check if a variable is an integer or float
+# TODO: Make this work better with units
+def is_number(x):
+    return type(x) == int or type(x) == float
